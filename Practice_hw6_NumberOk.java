3358d242846094715652442404bbacbf2baa343b
sangmi <97028831+sangmi24@users.noreply.github.com>
2022-03-05 23:36:31 +0900

    new files

---
+++ Practice_hw6_NumberOk.java
@@ -0,0 +1,58 @@
+package com.hw6.controller;
+
+import java.util.Scanner;
+
+public class NumberOk {
+
+	private int ran;
+	
+    public NumberOk() {
+    	
+    }
+    //사용자에게 숫자를 입력 받고 입력 받은 숫자가 임의의 난수와 비교했을때
+    //작은지, 큰지, 몇번 만에 맞추는지, 맞추면 계속 할지 끝낼지 묻는 메소드
+	public void numGame() {
+		
+		//난수 = 정답
+		int inputAnswer= (int)( (Math.random()*100)+1 );
+		
+		Scanner sc = new Scanner(System.in);
+		int num; //num은 사용자가 찍는 숫자의 변수 
+		int count=1; //count는 맞춘 횟수를 카운트하는 변수
+		
+		while(true){	
+			System.out.print("1부터 100사이의 정수를 하나 입력하세요: ");		
+			num = sc.nextInt(); //반복문 안에 있으면 여러번 무조건 반복
+			
+           //사용자가 입력한 숫자와 난수와 같은지, 작은지, 큰지 비교		 
+		 if(inputAnswer == num ) {
+			 System.out.println( count +"번 만에 맞췄습니다.");			 
+			 break;
+		 }else if (inputAnswer> num){
+			 System.out.println( num+"보다 큽니다. "+count+"번째 실패!!" );
+					
+		 }else {
+			 System.out.println( num+"보다 작습니다. "+count+"번째 실패!!" );						
+		 }
+		 count++;
+		  
+			//sc.close();
+			 
+		} 		
+		
+		System.out.println("계속 하시겠습니까?(y/n): ");
+		char answer =sc.nextLine().toUpperCase().charAt(0); //이부분에서 오류남. 왜? 나지?
+		if(answer=='Y') {
+	      System.out.println("계속합니다.");
+		}else if(answer=='N'){
+		System.out.println("게임을 종료합니다.");
+	    }else {
+		System.out.println("잘못 입력하셨습니다. 다시 입력해주세요.");
+		}
+		
+	
+	
+	 }
+	}
+	
+
---
+++ Practice_hw6_NumberRun.java
@@ -0,0 +1,18 @@
+package com.hw6.run;
+
+import com.hw6.controller.NumberOk;
+
+public class NumberRun {
+
+	public static void main(String[] args) {
+	
+		NumberOk no = new NumberOk();
+		//NumberOk 객체를 이용하여 메소드 실행
+		no.numGame();
+		
+		//계속하는지 판단하는 y,n 입력값 판단하기
+		
+
+	}
+
+}
